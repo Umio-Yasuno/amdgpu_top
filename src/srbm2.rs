9cf6a8480280c430996f8bc33c28b3ae85f50e47
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2023-03-03 13:42:59 +0900

    update srbm2 layout

--- src/srbm2.rs
+++ src/srbm2.rs
@@ -35,13 +35,17 @@ impl SRBM2 {
     pub fn stat(&self) -> String {
         format!(
             concat!(
+                "{vce0_name:<10} {vce0:3}%\n",
+                "{vce1_name:<10} {vce1:3}%\n",
                 "{sdma0_name:<10} {sdma0:3}%\n",
                 "{sdma1_name:<10} {sdma1:3}%\n",
                 "{sdma2_name:<10} {sdma2:3}%\n",
                 "{sdma3_name:<10} {sdma3:3}%\n",
-                "{vce0_name:<10} {vce0:3}%\n",
-                "{vce1_name:<10} {vce1:3}%\n",
             ),
+            vce0_name = "VCE0",
+            vce0 = self.vce0,
+            vce1_name = "VCE1",
+            vce1 = self.vce1,
             sdma0_name = "SDMA0",
             sdma0 = self.sdma0,
             sdma1_name = "SDMA1",
@@ -50,10 +54,6 @@ impl SRBM2 {
             sdma2 = self.sdma2,
             sdma3_name = "SDMA3",
             sdma3 = self.sdma3,
-            vce0_name = "VCE0",
-            vce0 = self.vce0,
-            vce1_name = "VCE1",
-            vce1 = self.vce1,
         )
     }
 }
