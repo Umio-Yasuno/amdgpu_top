9daeab9deabecb4b0f6d10ac2aa9e4065ab72deb
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2023-03-04 23:43:19 +0900

    use total_heap_size for VRAM_INFO

--- src/vram_usage.rs
+++ src/vram_usage.rs
@@ -1,8 +1,10 @@
 use libdrm_amdgpu_sys::AMDGPU::drm_amdgpu_memory_info;
 
 pub struct VRAM_INFO {
+    pub total_vram: u64,
     pub usable_vram: u64,
     pub usage_vram: u64,
+    pub total_gtt: u64,
     pub usable_gtt: u64,
     pub usage_gtt: u64,
 }
@@ -16,19 +18,23 @@ impl VRAM_INFO {
             ),
             vram_label = "VRAM",
             usage_vram = self.usage_vram.saturating_div(1024 * 1024),
-            total_vram = self.usable_vram.saturating_div(1024 * 1024),
+            total_vram = self.total_vram.saturating_div(1024 * 1024),
             gtt_label = "GTT",
             usage_gtt = self.usage_gtt.saturating_div(1024 * 1024),
-            total_gtt = self.usable_gtt.saturating_div(1024 * 1024),
+            total_gtt = self.total_gtt.saturating_div(1024 * 1024),
         )
     }
 }
 
 impl From<&drm_amdgpu_memory_info> for VRAM_INFO {
     fn from(info: &drm_amdgpu_memory_info) -> Self {
+        // usable_heap_size is not fixed.
+        // usable_heap_size = real_vram_size - pin_size - reserved_size
         Self {
-            usable_vram: info.vram.usable_heap_size, // (real_vram_size - pin_size - reserved_size)
+            total_vram: info.vram.total_heap_size,
+            usable_vram: info.vram.usable_heap_size, 
             usage_vram: info.vram.heap_usage,
+            total_gtt: info.gtt.total_heap_size,
             usable_gtt: info.gtt.usable_heap_size,
             usage_gtt: info.gtt.heap_usage,
         }
