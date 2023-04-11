cf0dc80bfda4aa0a6ba64d891c4026e808c66f02
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2023-04-12 06:27:30 +0900

    update Sensors viewer remove GPU_LOAD

--- src/stat/sensors.rs
+++ src/stat/sensors.rs
@@ -1,4 +1,4 @@
-use super::{DeviceHandle, Text, Opt};
+use super::{DeviceHandle, Text, Opt, PANEL_WIDTH};
 use libdrm_amdgpu_sys::{
     PCI,
     AMDGPU::SENSOR_INFO::*,
@@ -6,12 +6,13 @@ use libdrm_amdgpu_sys::{
 use std::fmt::{self, Write};
 use serde_json::{json, Map, Value};
 
-const SENSORS_LIST: [(SENSOR_TYPE, &str, u32); 7] = [
+const WIDTH: usize = PANEL_WIDTH / 2;
+const SENSORS_LIST: &'static [(SENSOR_TYPE, &str, u32)] = &[
     (SENSOR_TYPE::GFX_SCLK, "MHz", 1),
     (SENSOR_TYPE::GFX_MCLK, "MHz", 1),
-    (SENSOR_TYPE::GPU_TEMP, "C", 1000),
-    (SENSOR_TYPE::GPU_LOAD, "%", 1),
-    (SENSOR_TYPE::GPU_AVG_POWER, "W", 1),
+    // (SENSOR_TYPE::GPU_TEMP, "C", 1000),
+    // (SENSOR_TYPE::GPU_LOAD, "%", 1),
+    // (SENSOR_TYPE::GPU_AVG_POWER, "W", 1),
     (SENSOR_TYPE::VDDNB, "mV", 1),
     (SENSOR_TYPE::VDDGFX, "mV", 1),
 ];
@@ -21,17 +22,33 @@ pub struct Sensor {
     cur: PCI::LINK,
     max: PCI::LINK,
     bus_info: PCI::BUS_INFO,
+    power_cap_w: u32,
+    critical_temp: u32,
+    fan_max_rpm: u32,
     pub text: Text,
 }
 
 impl Sensor {
     pub fn new(pci_bus: &PCI::BUS_INFO) -> Self {
+        let power_cap_w = Self::parse_hwmon(pci_bus, "power1_cap").saturating_div(1_000_000);
+        let critical_temp = Self::parse_hwmon(pci_bus, "temp1_crit").saturating_div(1_000);
+        let fan_max_rpm = Self::parse_hwmon(pci_bus, "fan1_max");
+
         Self {
             cur: pci_bus.get_link_info(PCI::STATUS::Current),
             max: pci_bus.get_link_info(PCI::STATUS::Max),
             bus_info: pci_bus.clone(),
+            power_cap_w,
+            critical_temp,
+            fan_max_rpm,
             text: Text::default(),
         }
+    }
+
+    fn parse_hwmon(pci_bus: &PCI::BUS_INFO, file_name: &str) -> u32 {
+        pci_bus.get_hwmon_path()
+            .and_then(|hwmon_path| std::fs::read_to_string(hwmon_path.join(file_name)).ok())
+            .and_then(|file| file.trim_end().parse::<u32>().ok()).unwrap_or(0)
     }
 
     pub fn update_status(&mut self) {
@@ -39,35 +56,62 @@ impl Sensor {
     }
 
     pub fn print(&mut self, amdgpu_dev: &DeviceHandle) -> Result<(), fmt::Error> {
+        const NAME_LEN: usize = 10;
+        const VAL_LEN: usize = 5;
         self.text.clear();
         self.update_status();
 
         let mut c = 0;
 
-        for (sensor, unit, div) in &SENSORS_LIST {
+        for (sensor, unit, div) in SENSORS_LIST {
             let sensor_name = sensor.to_string();
 
             if let Ok(val) = amdgpu_dev.sensor_info(*sensor) {
                 c += 1;
                 let val = val.saturating_div(*div);
-                let ln = if (c % 2) == 0 { "\n" } else { "" };
-                write!(self.text.buf, " {sensor_name:<15} => {val:>6} {unit:3} {ln}")?;
+                write!(
+                    self.text.buf,
+                    " {:<WIDTH$} ",
+                    format!("{sensor_name:<NAME_LEN$} => {val:>VAL_LEN$} {unit:3}")
+                )?;
+                if (c % 2) == 0 { writeln!(self.text.buf)? };
             }
         }
         if (c % 2) == 1 { writeln!(self.text.buf)?; }
 
-        if let Some(power_cap) = self.get_power_cap() {
-            let power_cap = power_cap.saturating_div(1_000_000); // microWatts -> Watts
-            writeln!(self.text.buf, " {:<15} => {power_cap:>6} W", "PowerCap")?;
+        if let Ok(temp) = amdgpu_dev.sensor_info(SENSOR_TYPE::GPU_TEMP) {
+            writeln!(
+                self.text.buf,
+                " {name:<NAME_LEN$} => {temp:>VAL_LEN$} C (Crit. {crit} C)",
+                name = "GPU Temp",
+                temp = temp.saturating_div(1_000),
+                crit = self.critical_temp,
+            )?;
+        }
+
+        if let Ok(power) = amdgpu_dev.sensor_info(SENSOR_TYPE::GPU_AVG_POWER) {
+            writeln!(
+                self.text.buf,
+                " {name:<NAME_LEN$} => {power:>VAL_LEN$} W (Cap. {cap} W)",
+                name = "GPU Power",
+                power = power,
+                cap = self.power_cap_w,
+            )?;
         }
 
         if let Some(fan_rpm) = self.get_fan_rpm() {
-            writeln!(self.text.buf, " {:<15} => {fan_rpm:>6} RPM", "FAN1")?;
+            writeln!(
+                self.text.buf,
+                " {name:<NAME_LEN$} => {fan_rpm:>VAL_LEN$} RPM (Max. {max} RPM)",
+                name = "Fan",
+                fan_rpm = fan_rpm,
+                max = self.fan_max_rpm,
+            )?;
         }
 
         writeln!(
             self.text.buf,
-            " PCI ({pci_bus}) => Gen{cur_gen}x{cur_width:<2} @ Gen{max_gen}x{max_width:<2} (Max) ",
+            " PCI ({pci_bus}) => Gen{cur_gen}x{cur_width:<2} (Max. Gen{max_gen}x{max_width})",
             pci_bus = self.bus_info,
             cur_gen = self.cur.gen,
             cur_width = self.cur.width,
@@ -88,16 +132,6 @@ impl Sensor {
         }
     }
 
-    pub fn get_power_cap(&self) -> Option<u32> {
-        let power_cap_path = self.bus_info.get_hwmon_path()?.join("power1_cap");
-
-        if let Ok(power_cap) = std::fs::read_to_string(&power_cap_path) {
-            power_cap.trim_end().parse().ok()
-        } else {
-            None
-        }
-    }
-
     pub fn json_value(&self, amdgpu_dev: &DeviceHandle) -> Value {
         let mut m = Map::new();
 
@@ -109,7 +143,7 @@ impl Sensor {
             }),
         );
 
-        for (sensor, unit, div) in &SENSORS_LIST {
+        for (sensor, unit, div) in SENSORS_LIST {
             if let Ok(val) = amdgpu_dev.sensor_info(*sensor) {
                 let val = val.saturating_div(*div);
                 m.insert(
