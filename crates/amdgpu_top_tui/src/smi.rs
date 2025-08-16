976d4eacce05be8a8fb54206cdad362709e4666c
Umio Yasuno <coelacanth_dream@protonmail.com>
2025-08-16 21:20:23 +0900

    smi: update layout for the suspending device

--- crates/amdgpu_top_tui/src/smi.rs
+++ crates/amdgpu_top_tui/src/smi.rs
@@ -215,14 +215,7 @@ impl SuspendedSmiApp {
         if let [Some(did), Some(rid)] = [device_path.device_id, device_path.revision_id] {
             let _ = writeln!(
                 info_text.buf,
-                "#{index:<2} [{name:<20} ({did:#X}:{rid:#X})]| {pci}   | Suspended",
-                name = device_path.device_name,
-                pci = device_path.pci,
-            );
-        } else {
-            let _ = writeln!(
-                info_text.buf,
-                "#{index:<2} [{name:<20}]| {pci}   | Suspended",
+                "#{index:<2} [{name:<GPU_NAME_LEN$}]         | {pci}   | Suspended        |",
                 name = device_path.device_name,
                 pci = device_path.pci,
             );
